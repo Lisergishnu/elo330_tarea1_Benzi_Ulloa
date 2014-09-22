7c6883700686fb0ffa4dab7e3ef1193b719eb281
Lisergishnu <marco.benzi@alumnos.usm.cl>
2014-09-22 16:29:18 -0300

    Removing sanity checks

--- fn2u.sh
+++ fn2u.sh
@@ -4,6 +4,7 @@ workingDir="";
 option=0;
 if (( $# > 2 )); then
   echo "Usage: $0 [directory] [n]";
+  exit 1
 elif (( $# == 0 )); then
   #Default sin opciones
   #Solo analisis en directorio actual
@@ -20,18 +21,21 @@ fi
 
 #Analisis y/o operacion en $1
 if (( $option == 1 )); then
-  echo Solo analisis en $workingDir;
+  #echo Solo analisis en $workingDir;
   ls $workingDir | sed -n '/[áéíóúÁÉÍÓÚÑñ\ ]/p'
+  exit $? #Regresa el retorno de la operacion anterior
 elif (( $option == 2 )); then
-  echo Analisis y operacion en $workingDir;
+  #echo Analisis y operacion en $workingDir;
   ls $workingDir | sed -n '/[áéíóúÁÉÍÓÚÑñ\ ]/p' > /tmp/op$$
   #Transformar:
   while read archivo; do
     archivoSano="$(echo $archivo | sed 'y/áéíóúÁÉÍÓÚÑñ /aeiouAEIOUNn_/')"
-    echo Cambiando nombre de $archivo a $archivoSano
+    #echo Cambiando nombre de $archivo a $archivoSano
     mv "$workingDir/$archivo" "$workingDir/$archivoSano"
   done < /tmp/op$$
   echo Done.
+  exit $?
 else
   echo ERROR: Option unspecified: $option;
+  exit 1
 fi
